23af0bbc37cd26d82cc4b05f7e4fd885b0dd04bc
areblk <annika.rengfelt@scania.com>
2022-05-04 13:20:45 +0200

    test(cypress): Custom commands

--- sdds-demo-page/cypress/support/commands.js
+++ sdds-demo-page/cypress/support/commands.js
@@ -1,3 +1,82 @@
+Cypress.Commands.add('close', (element) => {
+    cy.get(element)
+    .click()
+})
+
+Cypress.Commands.add('addToCart', () => {
+    cy.get('[data-cy="add-to-cart"]').click()
+})
+
+Cypress.Commands.add('activeMenuItem', (menuItem) => {
+    menuItem === undefined ?
+    cy.get('.sdds-sidebar-nav__item a')
+        .should('not.have.class', 'active')
+    : ''
+})
+
+Cypress.Commands.add('checkHeader', (appName) => {
+    //Header features {application name}, {cart symbol}, {profile symbol} and {Scania symbol}. Cart symbol has no badge.
+    cy.get('.sdds-nav')
+        .should('be.visible')
+    cy.get('.sdds-nav__app-name')
+        .should('contain', `${appName}`)
+    cy.get('[data-cy="go-to-cart"]')
+        .should('be.visible')
+    cy.get('.sdds-nav__right > :nth-child(1)').
+        should('be.visible')
+    cy.get('sdds-badge')
+        .should('not.exist')
+    cy.get('.user-avatar')
+        .should('be.visible')
+    cy.get('.sdds-nav__app-logo')
+        .should('be.visible')
+    //TODO: Add link and url assertions
+})
+
+Cypress.Commands.add('sideMenuPageLinks', (length, activePage) => {
+    cy.get('.sdds-sidebar').should('be.visible')
+    
+    length !== undefined
+    ? cy.get('.sdds-sidebar-nav__item-link').should('have.length', length)
+    : ''
+    
+    activePage === undefined
+    ? ''
+    : activePage === false
+        ? noActiveMenuItem()
+        : cy.contains(activePage).should('have.class', 'active')
+})
+
+Cypress.Commands.add('topFooter', (numberOfSections, numberOfLinks, links) => {
+    cy.get('.sdds-footer-top')
+        .should('be.visible')
+    cy.get('.sdds-footer-title')
+        .should('have.length', numberOfSections)
+    cy.get('.sdds-footer-top a')
+        .should('have.length', numberOfLinks)
+        
+    links !== undefined ?
+        links.forEach(link => {
+        cy.get('.sdds-footer-top a')
+            .should('contain', link)
+            })
+            : ''
+})
+
+Cypress.Commands.add('mainFooter', (elements) => {
+    cy.get('.sdds-footer-main')
+        .should('be.visible')
+        
+        elements.forEach(element => {
+        cy.get('.sdds-footer-main')
+            .should('contain', element)
+        })
+        
+    cy.get('.sdds-footer-main-brand')
+        .should('have.css', 'background-image', 'url("https://cdn.digitaldesign.scania.com/logotype/1.0.0/scania_wordmark-white/scania-wordmark-white.svg")')
+})
+
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
